00a5547da41b5a77061270e8a95d51b165e55784
KADFWJ <930135305@qq.com>
2018-01-26 13:31:49 +0800

    0.0.1

--- upload.sh
+++ upload.sh
@@ -5,13 +5,34 @@ git pull origin master --tags
 git stash pop
 
 VersionString=`grep -E 's.version.*=' LMHomeServeice.podspec`
-VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+#VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
-NewVersionNumber=$(($VersionNumber + 1))
+VersionNumber="${VersionString##*=}"
+
+confirmed="n"
+NewVersionNumber=""
+echo "当前版本号：${VersionNumber}"
+getNewVersionNumber() {
+    read -p "输入新版本号: " NewVersionNumber
+
+    if test -z "$NewVersionNumber"; then
+        getNewVersionNumber
+    fi
+}
+
+echo -e "\n"
+while [ "$confirmed" != "y" -a "$confirmed" != "Y" ]
+do
+    if [ "$confirmed" == "n" -o "$confirmed" == "N" ]; then
+        getNewVersionNumber
+    fi
+    read -p "输入版本为${NewVersionNumber}，检查是否正确? (y/n):" confirmed
+done
+
 LineNumber=`grep -nE 's.version.*=' LMHomeServeice.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" LMHomeServeice.podspec
+sed -i "" "${LineNumber}s/${VersionNumber}/\"${NewVersionNumber}\"/g" LMHomeServeice.podspec
 
-echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+echo "current version is ${VersionNumber}, new version is \"${NewVersionNumber}\""
 
 git add .
 git commit -am ${NewVersionNumber}
